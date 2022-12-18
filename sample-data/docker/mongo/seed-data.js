7ab16c1d0fbefbf002a089b2d429da8ba83b2b97
D N <danuser2018@gmail.com>
2022-12-18 22:07:00 +0100

    Added a new docker file for a local mongodb

---
+++ sample-data/docker/mongo/seed-data.js
@@ -0,0 +1,6 @@
+db.prices.insertMany([
+  {"productId": "35455", "brandId": "1", "priceList": "1", "startDate": new Date("2020-06-14T00:00:00.000+02:00"), "endDate": new Date("2020-12-31T23:59:59.000+01:00"), "value": 35.5, "currency": "EUR", "priority": 0},
+  {"productId": "35455", "brandId": "1", "priceList": "2", "startDate": new Date("2020-06-14T15:00:00.000+02:00"), "endDate": new Date("2020-06-14T18:30:00.000+02:00"), "value": 25.45, "currency": "EUR", "priority": 1},
+  {"productId": "35455", "brandId": "1", "priceList": "3", "startDate": new Date("2020-06-15T00:00:00.000+02:00"), "endDate": new Date("2020-06-15T11:00:00.000+02:00"), "value": 35.5, "currency": "EUR", "priority": 1},
+  {"productId": "35455", "brandId": "1", "priceList": "4", "startDate": new Date("2020-06-15T16:00:00.000+02:00"), "endDate": new Date("2020-12-31T23:59:59.000+01:00"), "value": 38.95, "currency": "EUR", "priority": 1}
+]);
